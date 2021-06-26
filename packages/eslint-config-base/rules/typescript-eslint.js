8b15ad502544e0ef952581b8cf8443e4528cef8a
Ofadiman <ofadiman@gmail.com>
2021-06-26 16:06:31 +0200

    chore: update packages

--- packages/eslint-config-base/rules/typescript-eslint.js
+++ packages/eslint-config-base/rules/typescript-eslint.js
@@ -84,6 +84,7 @@ module.exports = {
     '@typescript-eslint/no-unnecessary-type-arguments': 'error',
     '@typescript-eslint/no-unnecessary-type-assertion': 'error',
     '@typescript-eslint/no-unnecessary-type-constraint': 'off',
+    '@typescript-eslint/no-unsafe-argument': 'error',
     '@typescript-eslint/no-unsafe-assignment': 'error',
     '@typescript-eslint/no-unsafe-call': 'error',
     '@typescript-eslint/no-unsafe-member-access': 'error',
